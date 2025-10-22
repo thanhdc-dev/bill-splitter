925776a09d696e08400a3971b787eb42c7132d9b
dbs-thanhdc <thanh.dinh@deltabrains.vn>
2025-10-22 10:47:38 +0700

    refactor: remove obsolete spec files for various components and directives

--- src/app/directives/thousand-separator.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ThousandSeparator } from './thousand-separator';
-
-describe('ThousandSeparator', () => {
-  it('should create an instance', () => {
-    const directive = new ThousandSeparator();
-    expect(directive).toBeTruthy();
-  });
-});
